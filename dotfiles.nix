3d968307c4dd64cdaf3cd49e184e3ff6c05f541c
Michael Leandersson <tripokey@gmail.com>
2016-05-30 08:17:02 +0200

    Added stack config

--- dotfiles.nix
+++ dotfiles.nix
@@ -8,6 +8,10 @@
         ln -sfn /home/tripokey/dotfiles/.gitconfig /home/tripokey/.gitconfig
         ln -sfn /home/tripokey/dotfiles/.Xresources /home/tripokey/.Xresources
         ln -sfn /home/tripokey/dotfiles/.tmux.conf /home/tripokey/.tmux.conf
+
+        mkdir -p /home/tripokey/.stack
+        chown tripokey /home/tripokey/.stack
+        ln -sfn /home/tripokey/dotfiles/stack.yaml /home/tripokey/.stack/config.yaml
       '';
       deps = [];
     };
