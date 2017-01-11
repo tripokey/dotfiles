ca5415fe033990a59f23b5430eeec2ebd5b83fe0
Michael Leandersson <tripokey@gmail.com>
2017-01-11 21:59:46 +0100

    Changed mod key in xmonad to win

--- dotfiles.nix
+++ dotfiles.nix
@@ -8,6 +8,7 @@
         ln -sfn /home/tripokey/dotfiles/.gitconfig /home/tripokey/.gitconfig
         ln -sfn /home/tripokey/dotfiles/.Xresources /home/tripokey/.Xresources
         ln -sfn /home/tripokey/dotfiles/.tmux.conf /home/tripokey/.tmux.conf
+        ln -sfn /home/tripokey/dotfiles/.xmonad /home/tripokey/.xmonad
 
         mkdir -p /home/tripokey/.stack
         chown tripokey /home/tripokey/.stack
