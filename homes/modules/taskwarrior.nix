eb83ede2012719ed3e43023cd657ae41d727b0bd
Michael Leandersson <tripokey@gmail.com>
2019-10-14 20:13:48 +0200

    added taskopen

--- homes/modules/taskwarrior.nix
+++ homes/modules/taskwarrior.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }:
 
 {
+  home = {
+    packages = with pkgs; [
+      taskopen
+    ];
+
+    file.taskopenrc = {
+      text = ''
+        TASKBIN='${pkgs.taskwarrior}/bin/task'
+        NOTES_FOLDER="\$HOME/.local/share/tasknotes/"
+        NOTES_EXT=".txt"
+        PATH_EXT=${pkgs.taskopen}/share/taskopen/scripts
+      '';
+
+      target = ".taskopenrc";
+    };
+  };
+
   programs = {
     taskwarrior = {
       enable = true;
