5e118e74deebf022dbc378a1aa97c476cf671bf9
Michael Leandersson <tripokey@gmail.com>
2019-10-15 22:17:38 +0200

    fixed rust package

--- homes/modules/rust.nix
+++ homes/modules/rust.nix
@@ -4,6 +4,9 @@
   home = {
     packages = with pkgs; [
       (pkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; })
+
+      # Needed until https://github.com/mozilla/nixpkgs-mozilla/issues/82 is fixed
+      pkgs.stdenv.cc
     ];
   };
 }
