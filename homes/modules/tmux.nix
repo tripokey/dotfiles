de8b56a24fbf485b88af599b2ad881a2fc397d79
Michael Leandersson <tripokey@gmail.com>
2019-10-20 22:32:37 +0200

    bind tmux copy mode to escape

--- homes/modules/tmux.nix
+++ homes/modules/tmux.nix
@@ -37,7 +37,10 @@
         setw -g monitor-activity on
         set -g visual-activity on
 
+        unbind [
         unbind p
+
+        bind Escape copy-mode
 
         bind-key -T copy-mode-vi 'v' send -X begin-selection
         bind-key -T copy-mode-vi 'y' send -X copy-selection-and-cancel
