0df9cf3175712898e460ea90ccebb7d1d93199fe
Michael Leandersson <tripokey@gmail.com>
2020-04-13 14:31:11 +0200

    added background image

--- homes/modules/i3.nix
+++ homes/modules/i3.nix
@@ -2,12 +2,17 @@
 
 let
   modifier = "Mod4";
+  wallpaper = builtins.fetchurl {
+    url = "https://msgpwebcdn.azureedge.net/ori/wp-content/uploads/2018/06/OriWotWWallpaper_1920x1080.jpg";
+    sha256 = "0h6gqzh7cmy85a6a5ac54pxpij826zy3nwhx1vhw8xb3nfip0dma";
+  };
 in
 {
   home = {
     packages = with pkgs; [
       libnotify
       i3lock
+      feh
     ];
   };
 
@@ -23,6 +28,13 @@ in
             "${modifier}+Return" = "exec xterm -e tmux -2";
             "${modifier}+L" = "exec i3lock -c 000000";
           };
+          startup = [
+            {
+              command = "${pkgs.feh}/bin/feh --bg-scale ${wallpaper}";
+              always = true;
+              notification = false;
+            }
+          ];
         };
 
         extraConfig = ''
