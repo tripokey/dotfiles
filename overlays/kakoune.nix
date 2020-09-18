acc82d77f982122fe3371921673ba658068b9c32
Michael Leandersson <tripokey@gmail.com>
2020-09-18 22:38:37 +0200

    Reverted kak-lsp version

--- overlays/kakoune.nix
+++ overlays/kakoune.nix
@@ -17,15 +17,4 @@ rec {
       plugins = with self.kakounePlugins; [ kak-fzf ];
     };
   });
-  kak-lsp = super.rustPlatform.buildRustPackage rec {
-    pname = "kak-lsp";
-    version = "8.0.0";
-    src = super.fetchFromGitHub {
-      owner = "ul";
-      repo = "kak-lsp";
-      rev = "v${version}";
-      sha256 = "0nka51szivwhlfkimjiyzj67nxh75m784c28ass6ihlfax631w9m";
-    };
-    cargoSha256 = "0wrfcg47sq24177ylz0iv7zm3hlrwhfg64f8nsh3isq5mm8civjp";
-  };
 }
