294a7305edf28cb20f541dfdb6253f5c78cdec00
Michael Leandersson <tripokey@gmail.com>
2021-07-10 21:06:49 +0200

    Pinned rust version at 1.52.1

--- homes/modules/rust.nix
+++ homes/modules/rust.nix
@@ -3,7 +3,7 @@
 {
   home = {
     packages = with pkgs; [
-      (pkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; })
+      ((pkgs.rustChannelOf { channel = "1.52.1"; }).rust.override { extensions = [ "rust-src" ]; })
 
       # Needed until https://github.com/mozilla/nixpkgs-mozilla/issues/82 is fixed
       pkgs.stdenv.cc
