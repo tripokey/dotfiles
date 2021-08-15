10b9562011b2cdfcfe73b3d1cb70fdea9dfa6bbe
Michael Leandersson <tripokey@gmail.com>
2021-08-15 13:25:38 +0200

    Added wsl home

---
+++ homes/wsl.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./modules/bat.nix
+    ./modules/direnv.nix
+    ./modules/fish.nix
+    ./modules/git.nix
+    ./modules/home.nix
+    ./modules/kakoune.nix
+    ./modules/skim.nix
+    ./modules/starship.nix
+    ./modules/taskwarrior.nix
+    ./modules/tmux.nix
+    ./modules/helix.nix
+  ];
+
+  programs.home-manager.enable = true;
+
+  home.username = "tripokey";
+  home.homeDirectory = "/home/tripokey";
+
+  home.stateVersion = "21.11";
+}
