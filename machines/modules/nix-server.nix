270f29e10201c0c84c734138b1cda4f2fc7950d1
Michael Leandersson <tripokey@gmail.com>
2022-04-16 11:12:29 +0200

    NixOS 21.11

--- machines/modules/nix-server.nix
+++ machines/modules/nix-server.nix
@@ -3,10 +3,13 @@
 {
   users.extraUsers.nixBuild = {
     name = "nixBuild";
+    group = "nixBuild";
     useDefaultShell = true;
     isSystemUser = true;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWeoa0zBduKGe7xs2zvNz0jLbqFlEpViEQ+QNfYB8wz nixBuild" ];
   };
+
+  users.groups.nixBuild = {};
 
   nix.trustedUsers = [ "nixBuild" ];
 }
