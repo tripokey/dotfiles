329bd3920d8dda4d91a93ebc8abbce8da068398d
Michael Leandersson <tripokey@gmail.com>
2022-11-20 12:31:30 +0100

    starship: nushell integration

--- homes/modules/starship.nix
+++ homes/modules/starship.nix
@@ -4,4 +4,21 @@
   programs.starship = {
     enable = true;
   };
+
+  home.file.nushell_env = {
+    text = ''
+    mkdir ~/.cache/starship
+    starship init nu | save ~/.cache/starship/init.nu
+    '';
+
+    target = ".config/nushell/env.nu";
+  };
+
+  home.file.nushell_config = {
+    text = ''
+    source ~/.cache/starship/init.nu
+    '';
+
+    target = ".config/nushell/config.nu";
+  };
 }
