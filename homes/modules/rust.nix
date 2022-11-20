ceb044cf3f035479d795c323a4620a7f76115ab2
Michael Leandersson <tripokey@gmail.com>
2022-11-20 12:14:25 +0100

    rust: switched to rustup

--- homes/modules/rust.nix
+++ homes/modules/rust.nix
@@ -3,10 +3,8 @@
 {
   home = {
     packages = with pkgs; [
-      ((pkgs.rustChannelOf { channel = "1.55.0"; }).rust.override { extensions = [ "rust-src" ]; })
-
-      # Needed until https://github.com/mozilla/nixpkgs-mozilla/issues/82 is fixed
       pkgs.stdenv.cc
+      rustup
     ];
   };
 }
