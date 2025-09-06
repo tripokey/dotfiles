3a2f1b5ff49cd8fcfdcb4559c6cb01b268c5bcc2
Michael Leandersson <tripokey@gmail.com>
2025-09-06 13:47:30 +0200

    Neovim config

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,33 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = false,
+  ft = "markdown",
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+    },
+  },
+  keys = {
+    { "<leader>o", "", desc = "+obsidian", mode = "n" },
+    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
+    { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link under cursor", mode = "n" },
+    { "<leader>op", "<cmd>ObsidianPasteImg<cr>", desc = "Paste image from clipboard under cursor", mode = "n" },
+    { "<leader>od", "<cmd>ObsidianDailies<cr>", desc = "Daily Obsidian notes", mode = "n" },
+    { "<leader>ow", "<cmd>ObsidianWorkspace<cr>", desc = "Switch to another workspace", mode = "n" },
+    { "<leader>os", "", desc = "+search", mode = "n" },
+    { "<leader>oss", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
+    { "<leader>osq", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
+    { "<leader>ost", "<cmd>ObsidianTags<cr>", desc = "Search Obsidian tags", mode = "n" },
+    { "<leader>osr", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
+    { "<leader>osl", "<cmd>ObsidianLinks<cr>", desc = "Show links", mode = "n" },
+    { "<leader>ot", "", desc = "+template", mode = "n" },
+    { "<leader>otn", "<cmd>ObsidianNewFromTemplate<cr>", desc = "New Obsidian note from template", mode = "n" },
+    { "<leader>oti", "<cmd>ObsidianTemplate<cr>", desc = "Insert template", mode = "n" },
+  },
+}
